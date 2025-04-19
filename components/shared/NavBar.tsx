f25aa6370c65c8f964d20ce647e33483f2ed2bd6
Aaron Rodriguez <69766780+AaronERodriguez@users.noreply.github.com>
2025-04-19 12:07:01 -0600

    implemented tailwind css v4

--- components/shared/NavBar.tsx
+++ components/shared/NavBar.tsx
@@ -5,8 +5,8 @@ type Props = {}
 
 const NavBar = (props: Props) => {
   return (
-    <div className='w-full flex p-3 px-10 flex-row justify-between bg-primary'>
-        <Link href='../' className='text-xl font-mono text-shado  '>
+    <div className='w-full flex h-10 px-10 flex-row justify-between bg-primary items-center'>
+        <Link href='../' className='text-xl font-mono text-shadow-border text-shadow-lg hover:scale-125 transition-transform duration-200 '>
             NavBar
         </Link>
     </div>
